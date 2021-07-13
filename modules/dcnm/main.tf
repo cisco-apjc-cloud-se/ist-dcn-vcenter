884d9737cd9a3d7a79b16912edbb61973df1ba4a
Richard <richwats@cisco.com>
2021-07-13 13:58:54 +1000

    merged test

--- modules/dcnm/main.tf
+++ modules/dcnm/main.tf
@@ -91,7 +91,8 @@ resource "dcnm_network" "tf-net-1" {
   deploy          = each.value.deploy
 
   dynamic "attachments" {
-    for_each = each.value.attachments
+    # for_each = each.value.attachments
+    for_each = local.merged
     content {
       serial_number = attachments.value["serial_number"]
       vlan_id = each.value.vlan_id
