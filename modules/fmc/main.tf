025ae376bbdfcb5b0a268b56c4b2b8bb574807d2
Richard <richwats@cisco.com>
2021-08-25 16:21:21 +1000

    test moving provider & depends

--- modules/fmc/main.tf
+++ modules/fmc/main.tf
@@ -1,19 +1,19 @@
-terraform {
-  required_providers {
-    fmc = {
-      source = "CiscoDevNet/fmc"
-      # version = "0.1.1"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     fmc = {
+#       source = "CiscoDevNet/fmc"
+#       # version = "0.1.1"
+#     }
+#   }
+# }
 
-provider "fmc" {
-  # Configuration options
-  fmc_username              = var.fmc_user
-  fmc_password              = var.fmc_password
-  fmc_host                  = var.fmc_server
-  fmc_insecure_skip_verify  = true
-}
+# provider "fmc" {
+#   # Configuration options
+#   fmc_username              = var.fmc_user
+#   fmc_password              = var.fmc_password
+#   fmc_host                  = var.fmc_server
+#   fmc_insecure_skip_verify  = true
+# }
 
 # locals {
 #   vm_group_a = {
